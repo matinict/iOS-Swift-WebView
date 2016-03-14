934d07eb598a6f8806c3740a3f67b9027c66de7b
Abdul Matin <matinict@gmail.com>
2016-03-14 11:13:07 +0600

    code upload
    code upload

---
+++ SSLCOMMERZ Panel/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  SSLCOMMERZ Panel
+//
+//  Created by SSL Wireless on 2/17/16.
+//  Copyright © 2016 SSL Wireless. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var sslcommerzPanelWebView: UIWebView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let myURL = NSURL(string: "https://merchant.sslcommerz.com");
+        let myURLRequest:NSURLRequest = NSURLRequest(URL: myURL!);
+        sslcommerzPanelWebView.loadRequest(myURLRequest);
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
